13c5edef39073eeae900c58101a8eb5c3d60615a
+ <+>
2023-06-15 16:29:08 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(distance) {
+    if (distance => 0) {
+        return Math.abs(distance - 42)
+    }
+  }
+
+function distanceFromHqInFeet(distance) {
+  function distanceFromHqInBlocks(distance) {
+    if (distance => 0) {
+        return Math.abs(distance - 42)
+    }
+  } return Math.abs(distance - 42) * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+  if (start, destination >= 0) {
+    return Math.abs (start - destination) * 264
+  }
+}
+
+function calculatesFarePrice(start, destination) {
+  let distance = Math.abs(start - destination) * 264
+  if (distance <= 400){
+      return 0;
+    }
+  else if (distance >= 400 && distance <= 2000){ 
+    return (distance - 400) * 0.02
+    }
+  else if ( distance > 2000 && distance <= 2500) {
+    return 25
+  } 
+  else if (distance >= 2500) {
+    return "cannot travel that far";
+  }
+}
+
+
+  
